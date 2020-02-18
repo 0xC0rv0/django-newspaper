ae7aedcfe4aaeea45cc1c2d536984d704cee5d8e
v01d57orm <crow7@protonmail.com>
2020-02-18 11:48:46 +0200

    CustomUser

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm,CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username','email', 'age', 'is_staff', ] # new 
+
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
-    
