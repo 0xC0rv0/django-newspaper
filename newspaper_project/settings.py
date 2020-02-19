9155e03a4b5600cbcb1184c8bd2b5922952cb77f
v01d57orm <crow7@protonmail.com>
2020-02-19 14:47:28 +0200

    reset password

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -132,4 +132,10 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # new
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
